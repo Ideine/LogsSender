cb34d65c7a79d88112eb4386d7267f475fc2f309
Christine Lasquellec <christine.lasquellec@ideine.fr>
2022-03-07 09:33:01 +0100

    Add LogException to Extensions

---
+++ Extensions/ContextLogServiceExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Ideine.LogsSender.Interfaces;
+
+namespace Ideine.LogsSender.Extensions
+{
+	public static class ContextLogServiceExtensions
+	{
+		public static void LogException(this IContextLogService contextLogService, LogLevel logLevel, Exception ex, string message)
+		{
+			contextLogService.Log(logLevel, x => x
+				.WriteException("exception", ex)
+				.WriteMethodInfo("context")
+				.WriteProperty("message", message));
+		}
+	}
+}
